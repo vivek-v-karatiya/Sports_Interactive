149fce4e0335ef001993cacdd67d3b86800293ad
vivek-s-katariya <vivekkatariya.study1@gmail.com>
2023-03-22 23:20:37 +0530

    create new react application

---
+++ react-application/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Sports Interactive Project Initialization</p>
+    </div>
+  );
+};
+
+export default App;
